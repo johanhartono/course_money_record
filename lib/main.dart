1c3c47bb1525f8d151dc9bf7c0624863927de3a9
johan <johan@sta.co.id>
2022-09-17 00:15:29 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,26 @@ import 'package:course_money_record/presentation/page/auth/login_page.dart';
 import 'package:course_money_record/presentation/page/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:io';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:window_size/window_size.dart';
+//import 'package:window_manager/window_manager.dart';
 
+//References Website : https://blog.codemagic.io/flutter-desktop-apps-intro/
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  //For Deskstop Platform only
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    //setWindowTitle('Flutter Demo');
+    setWindowMinSize(const Size(400, 300));
+    setWindowMaxSize(Size.infinite);
+  }
   initializeDateFormatting('id_ID').then((value) {
     runApp(const MyApp());
   });
+/*     windowManager.waitUntilReadyToShow().then((_) async{
+      await windowManager.setAsFrameless();
+  }); */
 }
 
 class MyApp extends StatelessWidget {
@@ -33,6 +46,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: FutureBuilder(
+        ///Retrieve session data
         future: Session.getUser(),
         builder: (context, AsyncSnapshot<User> snapshot) {
           if (snapshot.data != null && snapshot.data!.idUser != null) {
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,6 +3,9 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  screen_retriever
+  window_manager
+  window_size
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
